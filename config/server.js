af974458973ba9a22096214b9f1fe547e7a74141
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2020-12-21 21:36:56 +0100

    test

--- config/server.js
+++ config/server.js
@@ -1,10 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:1337"
-      : "https://api-boiler-strapi-k7c6n.ondigitalocean.app",
+  url: "https://api-boiler-strapi-k7c6n.ondigitalocean.app",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "976450683d7a665a5903818cb21e4ada"),
